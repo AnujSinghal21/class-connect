6184e0b560213bb2838be632d45b9a719b5ee714
Chinmay-A <chinmayaons1@gmail.com>
2023-12-15 16:36:43 +0530

    created a db and schemas

---
+++ database/dbstruct.js
@@ -0,0 +1,86 @@
+const connect=require('./connect.js')
+const mongoose=require('mongoose')
+
+const CourseSchema= new mongoose.Schema({
+    code:{
+        type: String,
+        required: true
+    },
+    department:{
+        type: String,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: false
+    },
+    prof:{
+        type: String,
+        required: true
+    },
+    oprof:{
+        type: String,
+        required: false,
+        default: "none"
+    },
+    profemail:{
+        type: String,
+        required: true
+    },
+    year:{
+        type: Number,
+        required: true
+    },
+    semester:{
+        type: String,
+        required: true
+    },
+    schedule:{
+        type: [String],
+        required: true
+    },
+    resources: String,
+    grades: String
+
+})
+
+const ProfSchema= new mongoose.Schema({
+    rating:{
+        type: Number,
+        required: true
+    },
+    department:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    comments:{
+        type: [String],
+        required: false,
+        default: []
+
+    },
+    courses:{
+        type: [String],
+        required: false
+
+    }
+})
+
+
+var Prof = mongoose.model('Professors',ProfSchema);
+var Course= mongoose.model('Course',CourseSchema);
+
+module.exports.Course=Course;
+module.exports.Prof=Prof;
