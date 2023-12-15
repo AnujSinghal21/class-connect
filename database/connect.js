9feb467a1a8a91d2b7b069a86d8b04dc5ad60dc0
Chinmay-A <chinmayaons1@gmail.com>
2023-12-15 23:00:53 +0530

    Collections Created, code organised and commented

--- database/connect.js
+++ database/connect.js
@@ -4,8 +4,12 @@ const PASS="dnzEcQDErO9QtM3E";
 
 const DBName="appDB";
 
+// The URI to connect to the DB
 URI=`mongodb+srv://classconnect2023:${PASS}@classconnect.yiphpts.mongodb.net/${DBName}?retryWrites=true&w=majority`;
 
+// Asynchronous function that awaits a connection
+//logs an error if connection is failed within 30 seconds(default)
+//use mongoose.connection() as a db object and then perform operations after calling connection function
 var connection= async ()=>{
 
   mongoose.connection.on('connected', () => console.log('connected with host: ',mongoose.connection.host));
