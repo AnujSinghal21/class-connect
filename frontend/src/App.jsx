26a4751df6998c2ebf15e08504bd76d82c7137d5
= <singhalanuj7575@gmail.com>
2023-12-15 12:31:36 +0530

    Created sidebar, added cdbreact library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,13 +13,18 @@ import AboutPage from "./pages/AboutPage"
 function App() {
   return (
     <Router>
-      <VerticalNav />
-      <Routes>
-          <Route exact path="/" element={<MainPage />} />
-          <Route path="/courses" element={<CoursePage />} />
-          <Route path="/profs" element={<ProfPage />} />
-          <Route path="/about" element={<AboutPage />} />
-      </Routes>
+      <div className="d-flex flex-row">
+        <VerticalNav />
+        <div className="m-2">
+          <Routes>
+              <Route exact path="/" element={<MainPage />} />
+              <Route path="/courses" element={<CoursePage />} />
+              <Route path="/profs" element={<ProfPage />} />
+              <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+
+      </div>
     </Router>
   )
 }
--- frontend/src/pages/VerticalNav.jsx
+++ frontend/src/pages/VerticalNav.jsx
@@ -1,12 +1,52 @@
-/* eslint-disable react/prop-types */
-import { Spinner } from 'reactstrap';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
 
-function VerticalNav(props) {
+const VerticalNav = () => {
   return (
-    <div className="fs-2 mt-5 text-center">
-      {props.message} <Spinner color={props.color ? props.color : "primary"}> Loading Content ... </Spinner>
-    </div>
-  )
-}
+    <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
+      <CDBSidebar textColor="#fff" backgroundColor="#333">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+            Class Connect
+          </a>
+        </CDBSidebarHeader>
 
-export default VerticalNav
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink to="/" activeclassname="activeClicked">
+              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink to="/courses" activeclassname="activeClicked">
+              <CDBSidebarMenuItem icon="book">Courses</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink to="/profs" activeclassname="activeClicked">
+              <CDBSidebarMenuItem icon="user">Professors</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink to="/about" activeclassname="activeClicked">
+              <CDBSidebarMenuItem icon="exclamation-circle">About Us</CDBSidebarMenuItem>
+            </NavLink>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            style={{
+              padding: '20px 5px',
+            }}
+          >
+            Copyright &copy; 2023<br /> No rights reserved.
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
+  );
+};
+
+export default VerticalNav;
