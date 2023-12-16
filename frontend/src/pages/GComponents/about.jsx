42b419a3fe6b9d054f1a65d0c99c3b3caed11001
Goutam-115 <goutam03579maths@gmail.com>
2023-12-16 13:20:23 +0530

    aboutimage1

--- frontend/src/pages/GComponents/about.jsx
+++ frontend/src/pages/GComponents/about.jsx
@@ -45,20 +45,20 @@ import image1 from './Aboutimage1.jpg'
 const items = [
   {
     src: image1,
-    altText: 'Slide 1',
-    caption: 'Slide 1',
+    // altText: 'Slide 1',
+    // caption: 'Slide 1',
     key: 1,
   },
   {
     src: 'https://picsum.photos/id/456/1200/400',
-    altText: 'Slide 2',
-    caption: 'Slide 2',
+    // altText: 'Slide 2',
+    // caption: 'Slide 2',
     key: 2,
   },
   {
     src: 'https://picsum.photos/id/678/1200/400',
-    altText: 'Slide 3',
-    caption: 'Slide 3',
+    // altText: 'Slide 3',
+    // caption: 'Slide 3',
     key: 3,
   },
 ];
