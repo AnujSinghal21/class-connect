5c258b8ec71894cec20768804ecbc4e4aa13a145
Goutam-115 <goutam03579maths@gmail.com>
2023-12-16 15:23:55 +0530

    about 1st

--- frontend/src/pages/GComponents/about.jsx
+++ frontend/src/pages/GComponents/about.jsx
@@ -1,37 +1,3 @@
-// // About.js
-// import React from 'react';
-// const About = () => {
-//   return (
-//     <div className="about-container">
-//       <div className="about-content">
-//         <h2>Welcome to Our Course Registration Platform</h2>
-//         <p>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus
-//           nunc id ante tincidunt, vel efficitur libero volutpat. Curabitur
-//           interdum metus eget facilisis fringilla.
-//         </p>
-//         <p>
-//           Our platform is designed to make the course registration process
-//           seamless for students. Explore the available courses, plan your
-//           schedule, and make informed decisions for the upcoming semester.
-//         </p>
-
-//         <h2>Meet Our Team</h2>
-//         <div className="team-members">
-//           {/* Team member cards go here */}
-//           <div className="team-member">
-//             <img src="john-doe.jpg" alt="John Doe" />
-//             <h4>John Doe</h4>
-//             <p>Frontend Developer</p>
-//           </div>
-//           {/* Add more team members as needed */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
 import React, { useState } from 'react';
 import {
   Carousel,
@@ -43,21 +9,21 @@ import {
 
 const items = [
   {
-    src: 'https://picsum.photos/id/456/1200/400',
-    // altText: 'Slide 1',
-    // caption: 'Slide 1',
+    src: 'https://picsum.photos/id/123/1200/400',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
     key: 1,
   },
   {
     src: 'https://picsum.photos/id/456/1200/400',
-    // altText: 'Slide 2',
-    // caption: 'Slide 2',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
     key: 2,
   },
   {
     src: 'https://picsum.photos/id/678/1200/400',
-    // altText: 'Slide 3',
-    // caption: 'Slide 3',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
     key: 3,
   },
 ];
