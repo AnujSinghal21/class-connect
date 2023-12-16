5c2d9344aad00d508d1452cbf485041ddc0ec0a4
Goutam-115 <goutam03579maths@gmail.com>
2023-12-16 14:57:42 +0530

    course first draft

--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import { Spinner } from 'reactstrap';
-
+import CourseP from './GComponents/CourseP'
 function CoursePage(props) {
   return (
     <div className="fs-2 mt-5 text-center">
-      {props.message} <Spinner color={props.color ? props.color : "primary"}> Loading Content ... </Spinner>
+      {/* {props.message} <Spinner color={props.color ? props.color : "primary"}> Loading Content ... </Spinner> */}
+      <CourseP/>
     </div>
   )
 }
--- frontend/src/pages/GComponents/CourseP.jsx
+++ frontend/src/pages/GComponents/CourseP.jsx
@@ -7,20 +7,23 @@ const coursesData = [
     id: 1,
     title: 'React Basics',
     content: 'Introduction to React and its fundamentals.',
+    review: 'Great course for beginners!',
   },
   {
     id: 2,
     title: 'Node.js for Beginners',
     content: 'Learn the basics of Node.js and server-side JavaScript.',
+    review: 'Excellent explanations and examples.',
   },
   // Add more courses as needed
 ];
 
-// Course component to display course details
+// Course component to display course details and review
 const Course = ({ course }) => (
   <div>
     <h2>{course.title}</h2>
     <p>{course.content}</p>
+    <p>Review: {course.review}</p>
   </div>
 );
 
@@ -42,7 +45,8 @@ const CourseP = () => {
     const matched = coursesData.filter(
       (course) =>
         course.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        course.content.toLowerCase().includes(searchQuery.toLowerCase())
+        course.content.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        course.review.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
     setMatchedCourses(matched);
@@ -50,16 +54,16 @@ const CourseP = () => {
 
   return (
     <div>
-      {/* Search input */}
-      <input
-        type="text"
-        placeholder="Search for a course..."
-        value={searchQuery}
-        onChange={handleSearchChange}
-      />
-
-      {/* Search button */}
-      <button onClick={handleSearchClick}>Search</button>
+      {/* Search input and button */}
+      <div>
+        <input
+          type="text"
+          placeholder="Search for a course..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button onClick={handleSearchClick}>Search</button>
+      </div>
 
       {/* Display matched courses */}
       {matchedCourses.map((course) => (
