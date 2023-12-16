81f41973b45ceb5e9ace917c9cb116990c6d7180
Goutam-115 <goutam03579maths@gmail.com>
2023-12-16 15:49:59 +0530

    about 1st

--- frontend/src/pages/GComponents/about.jsx
+++ frontend/src/pages/GComponents/about.jsx
@@ -1,96 +1,126 @@
-import React, { useState } from 'react';
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
-} from 'reactstrap';
-//import Aboutpage1 from './Aboutpage1'
-import Aboutpage1image from './Aboutpage1image.jpg'
-const items = [
-  {
-    src: Aboutpage1image,
-    altText: 'During pre-registration, many students feel difficulties while choosing department and open electives for their upcoming semester. It is a humongous task to find a course of your liking which doesn’t clash with your template from the huge DOAA course schedule. Also while choosing courses, grading of the corresponding professors are also an important factor, for which we search many sites and seek review from many seniors. To overcome all these problems, here is a single platform, which will answer all your queries and find suitable courses for you in just few minutes ...' ,
-    caption: 'Slide 1',
-    key: 1,
-  },
-  {
-    src: 'https://picsum.photos/id/456/1200/400',
-    altText: 'Slide 2',
-    caption: 'Slide 2',
-    key: 2,
-  },
-  {
-    src: 'https://picsum.photos/id/678/1200/400',
-    altText: 'Slide 3',
-    caption: 'Slide 3',
-    key: 3,
-  },
-];
+// import React, { useState } from 'react';
+// import {
+//   Carousel,
+//   CarouselItem,
+//   CarouselControl,
+//   CarouselIndicators,
+//   CarouselCaption,
+// } from 'reactstrap';
+// //import Aboutpage1 from './Aboutpage1'
+// //import Aboutpage1image from './Aboutpage1image.jpg'
+// const items = [
+//   {
+//     src: 'https://picsum.photos/id/456/1200/400',
+//     // altText: 'Slide1' ,
+//     // caption: 'Slide 1',
+//     key: 1,
+//   },
+//   {
+//     src: 'https://picsum.photos/id/456/1200/400',
+//     altText: 'Slide 2',
+//     caption: 'Slide 2',
+//     key: 2,
+//   },
+//   {
+//     src: 'https://picsum.photos/id/678/1200/400',
+//     altText: 'Slide 3',
+//     caption: 'Slide 3',
+//     key: 3,
+//   },
+// ];
 
-function Example(args) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
+// function Example(args) {
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const [animating, setAnimating] = useState(false);
 
-  const next = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-    setActiveIndex(nextIndex);
-  };
+//   const next = () => {
+//     if (animating) return;
+//     const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+//     setActiveIndex(nextIndex);
+//   };
 
-  const previous = () => {
-    if (animating) return;
-    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-    setActiveIndex(nextIndex);
-  };
+//   const previous = () => {
+//     if (animating) return;
+//     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+//     setActiveIndex(nextIndex);
+//   };
 
-  const goToIndex = (newIndex) => {
-    if (animating) return;
-    setActiveIndex(newIndex);
-  };
+//   const goToIndex = (newIndex) => {
+//     if (animating) return;
+//     setActiveIndex(newIndex);
+//   };
 
-  const slides = items.map((item) => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-        <img src={item.src} alt={item.altText} />
-        <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
-        />
-      </CarouselItem>
-    );
-  });
+//   const slides = items.map((item) => {
+//     return (
+//       <CarouselItem
+//         onExiting={() => setAnimating(true)}
+//         onExited={() => setAnimating(false)}
+//         key={item.src}
+//       >
+//         <img src={item.src} alt={item.altText} />
+//         <CarouselCaption
+//           captionText={item.caption}
+//           captionHeader={item.caption}
+//         />
+//       </CarouselItem>
+//     );
+//   });
 
+//   return (
+//     <Carousel
+//       activeIndex={activeIndex}
+//       next={next}
+//       previous={previous}
+//       {...args}
+//     >
+//       <CarouselIndicators
+//         items={items}
+//         activeIndex={activeIndex}
+//         onClickHandler={goToIndex}
+//       />
+//       {slides}
+//       <CarouselControl
+//         direction="prev"
+//         directionText="Previous"
+//         onClickHandler={previous}
+//       />
+//       <CarouselControl
+//         direction="next"
+//         directionText="Next"
+//         onClickHandler={next}
+//       />
+//     </Carousel>
+//   );
+// }
+
+// export default Example;
+// AboutPage.js
+
+import React from 'react';
+import './About.css'; // Make sure to create an AboutPage.css file for styling
+
+const About = () => {
   return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-      {...args}
-    >
-      <CarouselIndicators
-        items={items}
-        activeIndex={activeIndex}
-        onClickHandler={goToIndex}
-      />
-      {slides}
-      <CarouselControl
-        direction="prev"
-        directionText="Previous"
-        onClickHandler={previous}
-      />
-      <CarouselControl
-        direction="next"
-        directionText="Next"
-        onClickHandler={next}
-      />
-    </Carousel>
+    <div className="about-container">
+      <h1 className="about-heading">About Us</h1>
+      <div className="content-box">
+        <p>
+          Welcome to our website! We are a passionate team dedicated to providing
+          quality content and services. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
+        </p>
+        <p>
+          Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
+          Fusce nec tellus sed augue semper porta.
+        </p>
+      </div>
+      <h2 className="sub-heading">Our Mission</h2>
+      <p>
+        Our mission is to deliver valuable and innovative solutions to our users.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
   );
 }
 
-export default Example;
+export default About;
