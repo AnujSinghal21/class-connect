757d773fd9e3497b5b148c9b7809def3d20facc4
Chinmay-A <chinmayaons1@gmail.com>
2023-12-17 09:50:00 +0530

    added API to insert profs and courses

--- api/api.js
+++ api/api.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const {Course,Prof}=require("../database/models.js")
 
 //the api to getCourses using custom parameters
@@ -9,10 +10,69 @@ module.exports.getCourses=(request,response)=>{
     Course.find(params).then(value=>{response.send(value)}).catch(error=>{console.log(error)});
 
 };
+
 module.exports.getProfs=(request,response)=>{
     let params=request.query;
     console.log(params);
     Prof.find(params).then(value=>{response.send(value)}).catch(error=>{console.log(error)});
 };
 
+module.exports.AddCourse=(request,response)=>{
+    let params=request.query;
+    console.log(params);
 
+    let new_course_data={
+        code: params.code,
+        department: params.department,
+        title:params.title,
+        content:params.content || "",
+        prof:params.prof,
+        oprof:params.oprof || "",
+        profemail: params.profemail,
+        year: params.year,
+        semester: params.semester,
+        schedule: params.schedule,
+        resources: params.resource || "Not Available",
+        grades: params.grades || "Not Availalbe"
+    };
+
+    let new_course=new Course(new_course_data);
+
+    try{
+        new_course.save();
+        response.status(200).send(params);
+    }
+    catch(error)
+    {
+        console.log(error);
+        response.send(500).send("Internal Server Error");
+    }
+    
+};
+
+module.exports.AddProf=(request,response)=>{
+    
+    let params=request.query;
+
+    console.log(params);
+
+    let prof_data={
+        name: params.name,
+        department: params.department,
+        email: params.email,
+        comments: params.comments || ["None"],
+        courses: params.courses || [],
+    }
+
+    let new_prof= new Prof(prof_data);
+
+    try{
+        new_prof.save();
+        response.status(200).send(params);
+    }
+    catch(error)
+    {
+        console.log(error);
+        response.status(500);
+    }
+};
--- api/router.js
+++ api/router.js
@@ -2,7 +2,9 @@ const express=require('express')
 const api=require('./api.js')
 const router=express.Router();
 
-router.get('/courses',api.getCourses);
-router.get('/profs',api.getProfs);
+router.get('/courses', api.getCourses);
+router.get('/profs', api.getProfs);
+router.post('/courses', api.AddCourse);
+router.post('/profs',api.AddProf);
 
 module.exports=router;
