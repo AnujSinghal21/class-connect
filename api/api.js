3a7e71ea5754ca8cddad80231de8be15216dc7c8
Chinmay-A <chinmayaons1@gmail.com>
2023-12-22 19:08:44 +0530

    removed ip filtering

--- api/api.js
+++ api/api.js
@@ -128,28 +128,28 @@ module.exports.addCourseComment=async (request,response)=>{
 
     let params=request.query;
 
-    console.log(params.id);
+    //console.log(params.id);
 
-    let check_exist=await Course.find({_id: params.id, comments: {$elemMatch: {ip: request.ip}}});
+    //let check_exist=await Course.find({_id: params.id, comments: {$elemMatch: {ip: request.ip}}});
 
-    if(check_exist.length==0)
-    {
-        try
-        {
+    //if(check_exist.length==0)
+    //{
+    //    try
+    //    {
             final_value= await Course.findByIdAndUpdate(params.id,{$push:{comments:{ comment: params.comment, ip: request.ip}}},{new: true});
             response.status(200).send(final_value);
-        }
-        catch(error)
-        {
-            console.log("here");
-            console.log(error);
-            response.status(500);
-        }
-    }
-    else
-    {
-        response.status(403).send("Comment is already given");
-    }
+    //    }
+    //    catch(error)
+    //    {
+    //        console.log("here");
+    //        console.log(error);
+    //       response.status(500);
+    //    }
+    //}
+    //else
+    //{
+    //    response.status(403).send("Comment is already given");
+    //}
 };
 
 //adding a comment to a professor
@@ -158,17 +158,17 @@ module.exports.addProfComment=async (request,response)=>{
 
     let params=request.query;
 
-    let check_exist=await Prof.find({_id: params.id, comments: {$elemMatch: {ip: request.ip}}});
-    console.log(check_exist);
-    if(check_exist.length==0)
-    {
+    //let check_exist=await Prof.find({_id: params.id, comments: {$elemMatch: {ip: request.ip}}});
+    //console.log(check_exist);
+    //if(check_exist.length==0)
+    //{
         final_value= await Prof.findByIdAndUpdate(params.id,{$push:{comments:{ comment: params.comment, ip: request.ip}}},{new: true});
         response.status(200).send(final_value);
-    }
-    else
-    {
-        response.status(403).send("Comment is already given");
-    }
+    //}
+    //else
+    //{
+    //    response.status(403).send("Comment is already given");
+    //}
 };
 
 //update course ratings
@@ -178,17 +178,17 @@ module.exports.rateCourse=async (request,response)=>{
 
     //console.log(params);
 
-    let check_exist=await Course.find({_id: params.id, ratings: {$elemMatch: {ip: request.ip}}});
-    console.log(check_exist);
-    if(check_exist.length==0)
-    {
+    //let check_exist=await Course.find({_id: params.id, ratings: {$elemMatch: {ip: request.ip}}});
+    //console.log(check_exist);
+    //if(check_exist.length==0)
+    //{
         final_value= await Course.findByIdAndUpdate(params.id,{$inc:{ ratingsum: params.ratingsum, ratingcount: 1}, $push:{ratings:{ rating: params.ratingsum, ip: request.ip}}},{new: true});
         response.status(200).send(final_value);
-    }
-    else
-    {
-        response.status(403).send("Rating is already given");
-    }
+    //}
+    //else
+    //{
+    //    response.status(403).send("Rating is already given");
+    //}
     //console.log(final_value);
     
 }
@@ -200,15 +200,15 @@ module.exports.rateProf=async (request,response)=>{
 
     //console.log(params);
 
-    let check_exist=await Prof.find({_id: params.id, ratings: {$elemMatch: {ip: request.ip}}});
-    console.log(check_exist);
-    if(check_exist.length==0)
-    {
+    // let check_exist=await Prof.find({_id: params.id, ratings: {$elemMatch: {ip: request.ip}}});
+    // console.log(check_exist);
+    // if(check_exist.length==0)
+    // {
         final_value= await Prof.findByIdAndUpdate(params.id,{$inc:{ ratingsum: params.ratingsum, ratingcount: 1}, $push:{ratings:{ rating: params.ratingsum, ip: request.ip}}},{new: true});
         response.status(200).send(final_value);
-    }
-    else
-    {
-        response.status(403).send("Rating is already given");
-    }
+    // }
+    // else
+    // {
+    //     response.status(403).send("Rating is already given");
+    // }
 }
